ca43f398dd63ecba3060f7302d95a63c8753e778
omer117 <omer.asraf91@gmail.com>
2023-01-18 11:34:01 +0200

    fixx

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -16,13 +16,4 @@ export class OrdersController {
     return this.ordersService.deleteAll();
   }
 
-  @Get()
-  findAll() {
-    return this.ordersService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.ordersService.findOne(+id);
-  }
 }
