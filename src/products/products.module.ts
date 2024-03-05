86afa5e23ed50f6012c4d17a63dd9333def8ac73
omer117 <omer.asraf91@gmail.com>
2024-03-05 15:53:15 +0200

    fix 2

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
   imports:[TypeOrmModule.forFeature([Product])],
