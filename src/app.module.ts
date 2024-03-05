aefe6dd6b6cc15ff141ce351b53078264d631580
omer117 <omer.asraf91@gmail.com>
2024-03-05 15:44:41 +0200

    fixed server and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,37 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './Products/Products.module';
 import { OrdersModule } from './orders/orders.module';
 import { DailyForecastModule } from './daily_forecast/daily_forecast.module';
-import { BeachesModule } from './beaches/beaches.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Product } from './products/entities/product.entity';
+import { BeachesModule } from './beaches/beaches.module';
 import { UsersModule } from './users/users.module';
 
+require('dotenv').config();
+let { PGURL, PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    url:'postgres://shaka_nxac_user:KneKPMHwl3Y4hsV8Nzxcm29XR6o4dFcT@dpg-citf3qp5rnuhcntnc98g-a.oregon-postgres.render.com/shaka_nxac',
-    type:'postgres',
-    host:'dpg-citf3qp5rnuhcntnc98g-a',
-    port:5432,
-    username:'shaka_nxac_user',
-    password:'KneKPMHwl3Y4hsV8Nzxcm29XR6o4dFcT',
-    database:'shaka_nxac',
-    autoLoadEntities:true,
-    synchronize:true,
-    ssl:{
-      rejectUnauthorized:false,
-      
-    }
-  }),UsersModule,ProductsModule, 
-  OrdersModule,
-   DailyForecastModule, BeachesModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ProductsModule,
+    OrdersModule,
+    DailyForecastModule,
+    TypeOrmModule.forRoot({
+      url: PGURL,
+      type: 'postgres',
+      host: PGHOST,
+      port: 5432,
+      username: PGUSER,
+      password: PGPASSWORD,
+      database: PGDATABASE,
+      autoLoadEntities: true,
+      ssl: true,
+      extra: {
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    }),
+    BeachesModule,
+    UsersModule
+  ],
 })
 export class AppModule { }
